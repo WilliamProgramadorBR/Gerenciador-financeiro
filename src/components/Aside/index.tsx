748b1177823c875521115804941fc865761079ea
William/Back-end <108165232+WilliamProgramadorBR@users.noreply.github.com>
2024-08-05 10:39:33 -0300

    Comitt de segurança

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,24 +1,92 @@
-import React from 'react';
-import { Container,Header,LogImg, MenuContainer, MenuItemLink, Title } from './styles';
-import logoImg from '../../assets/logo.svg'
-import {MdDashboard, MdArrowUpward, MdArrowDownward, MdExitToApp, MdMenu } from 'react-icons/md'
+import React, {useState} from 'react';
+import Toggle from '../Toggle';
 
-const Aside: React.FC = () =>{
+import {
+    MdDashboard,
+    MdArrowDownward,
+    MdArrowUpward,
+    MdExitToApp,
+    MdClose,
+    MdMenu, 
+} from 'react-icons/md';
 
-    return(
-        <Container>
-       <Header>
-        <LogImg  src={logoImg} alt="Logo minha carteira" />
-        <Title>Minha finanças</Title>
-       </Header>
-       <MenuContainer>
-        <MenuItemLink href="/"> 
-        <MdDashboard />Dashboard
-         </MenuItemLink>
-        <MenuItemLink href="/list/entry-balance"><MdArrowUpward/>Entrada  </MenuItemLink>
-        <MenuItemLink href="/list/exit-balance"><MdArrowDownward/>Saidas  </MenuItemLink>
-        <MenuItemLink href="#"> <MdExitToApp/>Sair </MenuItemLink>
-       </MenuContainer>
+import logoImg from '../../assets/logo.svg';
+
+import { useAuth } from '../../hooks/auth';
+import { useTheme } from '../../hooks/theme';
+
+import { 
+    Container,
+    Header,
+    LogImg,
+    Title,
+    MenuContainer,
+    MenuItemLink,
+    MenuItemButton,
+    ToggleMenu,
+    ThemeToggleFooter,
+}  from './styles';
+
+const Aside: React.FC = () => {
+    const { signOut } = useAuth();
+    const { toggleTheme, theme } = useTheme();
+
+    const [toggleMenuIsOpened, setToggleMenuIsOpened ] = useState(false);
+    const [darkTheme, setDarkTheme] = useState(() => theme.title === 'dark' ? true : false);
+
+
+    const handleToggleMenu = () => {
+        setToggleMenuIsOpened(!toggleMenuIsOpened);
+    }
+
+
+    const handleChangeTheme = () => {
+        setDarkTheme(!darkTheme);
+        toggleTheme();
+    }
+
+
+    return (
+        <Container menuIsOpen={toggleMenuIsOpened}>
+            <Header>
+                <ToggleMenu onClick={handleToggleMenu}>
+                { toggleMenuIsOpened ? <MdClose /> : <MdMenu /> }
+                </ToggleMenu>
+
+                <LogImg src={logoImg} alt="Logo Minha Carteira" />
+                <Title>Minha Carteira</Title>
+            </Header>
+
+            <MenuContainer>
+                <MenuItemLink href="/">
+                    <MdDashboard />
+                    Dashboard
+                </MenuItemLink>
+
+                <MenuItemLink href="/list/entry-balance">
+                    <MdArrowUpward />
+                    Entradas
+                </MenuItemLink>
+
+                <MenuItemLink href="/list/exit-balance">
+                    <MdArrowDownward />
+                    Saídas
+                </MenuItemLink>
+
+                <MenuItemButton onClick={signOut}>
+                    <MdExitToApp />
+                    Sair
+                </MenuItemButton>
+            </MenuContainer>
+
+            <ThemeToggleFooter menuIsOpen={toggleMenuIsOpened}>
+                <Toggle
+                    labelLeft="Light"
+                    labelRight="Dark"
+                    checked={darkTheme}
+                    onChange={handleChangeTheme}
+                />
+            </ThemeToggleFooter>
         </Container>
     );
 }
--- src/components/Aside/styles.ts
+++ src/components/Aside/styles.ts
@@ -1,9 +1,18 @@
-import styled from "styled-components";
+import styled, { css } from 'styled-components';
 
 
+interface IContainerProps {
+    menuIsOpen: boolean;
+}
 
-export const Container = styled.div`
-  grid-area: AS;
+
+interface IThemeToggleFooterProps {
+    menuIsOpen: boolean;
+}
+
+
+export const Container = styled.div<IContainerProps>`
+    grid-area: AS;
     
     background-color: ${props => props.theme.colors.secondary};
     padding-left: 20px;
@@ -19,7 +28,13 @@ export const Container = styled.div`
 
         width: 170px;
 
-     
+        height: ${props => props.menuIsOpen ? '100vh' : '70px'};
+        overflow: hidden;
+
+        ${props => !props.menuIsOpen && css`
+            border: none;
+            border-bottom: 1px solid ${props => props.theme.colors.gray};
+        `};
     }
 `;
 
@@ -79,3 +94,61 @@ export const MenuItemLink = styled.a`
 
 
 
+export const MenuItemButton = styled.button`
+    font-size: 16px;
+    color: ${props => props.theme.colors.info};
+    
+    border: none;
+    background: none;
+
+    margin: 7px 0;
+    display: flex;
+    align-items: center;
+
+    transition: opacity .3s;
+
+    &:hover {
+        opacity: .7;
+    }
+
+    > svg {
+        font-size: 18px;
+        margin-right: 5px;
+    }
+`;
+
+export const ToggleMenu = styled.button`
+    width: 40px;
+    height: 40px;
+
+    border-radius: 5px;
+    font-size: 22px;
+    
+    background-color: ${props => props.theme.colors.warning};
+    color: ${props => props.theme.colors.white};
+
+    transition: opacity .3s;
+
+    &:hover{
+        opacity: 0.7;
+    }
+
+    display: none;
+
+    @media(max-width: 600px){
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+`;
+
+export const ThemeToggleFooter = styled.footer<IThemeToggleFooterProps>`
+    display: none;
+    position: absolute;
+    bottom: 30px;
+    
+    @media(max-width: 470px){
+        display: ${props => props.menuIsOpen ? 'flex' : 'none'};
+    }
+
+`;
